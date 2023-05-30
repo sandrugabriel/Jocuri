f56fc45f66d8cae3fd42dc57095ca3d6b8afca93
Sandru Gabriel <gabi200611@yahoo.com>
2023-05-30 18:51:53 +0300

    done

---
+++ Jocuri/Controllers/ControllerUtilizatori.cs
@@ -0,0 +1,42 @@
+﻿using Jocuri.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jocuri.Controllers
+{
+    internal class ControllerUtilizatori
+    {
+
+        private List<Utilizator> utilizatori;
+
+        public ControllerUtilizatori()
+        {
+
+            utilizatori = new List<Utilizator>();
+
+            load();
+        }
+
+        public void load() {
+
+            string path = Application.StartupPath + @"/data/Utilizatori.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            string t;
+
+            while((t = streamReader.ReadLine()) != null)
+            {
+
+                Utilizator utilizator = new Utilizator(t);
+                utilizatori.Add(utilizator);
+            }
+        streamReader.Close();
+        }
+
+    }
+}
---
+++ Jocuri/Models/Rezultat.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jocuri.Models
+{
+    internal class Rezultat
+    {
+
+        private int id;
+        private int tipJoc;
+        private int idUtilizator;
+        private int punctraj;
+        private DateTime data;
+
+        public Rezultat(int id, int tipJoc, int idUtilizator, int punctraj, DateTime data)
+        {
+            this.id = id;
+            this.tipJoc = tipJoc;
+            this.idUtilizator = idUtilizator;
+            this.punctraj = punctraj;
+            this.data = data;
+        }
+
+        public Rezultat(string textul) {
+
+            string[] prop = textul.Split(';');
+
+            this.id = int.Parse(prop[0]);
+            this.tipJoc = int.Parse(prop[1]);
+            this.idUtilizator = int.Parse(prop[2]);
+            this.punctraj= int.Parse(prop[3]);
+            this.data = DateTime.Parse(prop[4]);
+        
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public int getTipJoc()
+        {
+            return tipJoc;
+        }
+
+        public int getIdUtilizator()
+        {
+            return idUtilizator;
+        }
+
+        public int getpunctaj()
+        {
+            return punctraj;
+        }
+
+        public DateTime getData()
+        {
+            return data;
+        }
+
+
+    }
+}
