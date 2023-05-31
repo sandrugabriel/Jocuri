5d55283f20380ab782bebceefac902d2f541b8ff
Sandru Gabriel <gabi200611@yahoo.com>
2023-05-31 10:05:01 +0300

    done

--- Jocuri/Form1.cs
+++ Jocuri/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Jocuri.Panels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,29 @@ namespace Jocuri
         public Form1()
         {
             InitializeComponent();
+
+           this.Controls.Add(new PnlAutentificare(this));
+
         }
+
+        public void removepnl(string pnl)
+        {
+
+            Control control = null;
+
+            foreach (Control c in this.Controls)
+            {
+                if (c.Name.Equals(pnl))
+                {
+                    control = c;
+                }
+
+            }
+
+            this.Controls.Remove(control);
+
+        }
+
+
     }
 }
